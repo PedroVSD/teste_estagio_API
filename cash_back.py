179c912e0130d5d78401eb061c0ea664637f7446
PedroVSD-linux <pedrovsd00@gmail.com>
2026-04-12 14:28:56 -0300

    Alterando o código para melhor uso

--- cash_back.py
+++ cash_back.py
@@ -3,25 +3,36 @@
 # vip tem 10% de bonus sobre o base que é 5%
 # Compras acima de 500 reais o cashback dobra, ou seja cashback final * 2
 
+def desconto(preco: float, percentual_desconto: float) -> float:
+    preco_desconto = preco * percentual_desconto
+    preco_final = preco - preco_desconto
+    return preco_final
 
+def cash_back(preco_original: float, cupom: float, cliente: str) -> float:
+    preco_final = desconto(preco_original, cupom)
 
-def cash_back(preco_original_final: float, cliente: str) -> float:
-    cash_back_base = preco_original_final * 0.05
+    cash_back_base = preco_final * 0.05
 
-    if cliente == 'VIP':
+    if cliente == 'VIP' or 'Vip' or 'vip':
         cash_back_atual = cash_back_base * 1.10
     else:
         cash_back_atual = cash_back_base
 
-    if preco_original_final > 500.00:
+    if preco_final > 500.00:
         cash_back_final = cash_back_atual * 2
     else:
         cash_back_final = cash_back_atual
 
     return round(cash_back_final, 2)
 
+#print(cash_back(600, 0.2, 'VIP'))
+#print(cash_back(600, 0.1, 'VIP'))
+#print(cash_back(600, 0.2, 'Normal'))
+#print(cash_back(600, 0.1, 'Normal'))
 
-print(cash_back(480, 'VIP'))
-print(cash_back(540, 'VIP'))
-print(cash_back(480, 'Normal'))
-print(cash_back(540, 'Normal'))
+print(cash_back(600, 0.15, 'VIP'))
+
+#26.4
+#59.4
+#24.0
+#54.0
