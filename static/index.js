7e7d39e8de7e7a08cb5a8c059d80854875801c73
PedroVSD-linux <pedrovsd00@gmail.com>
2026-04-14 21:02:27 -0300

    front para API

--- static/index.js
+++ static/index.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = "http://127.0.0.1:8000";
+const API_BASE_URL = "https://teste-estagio-api.onrender.com";
 
 const form = document.getElementById("cashbackForm");
 const resultadoBox = document.getElementById("resultado-box");
